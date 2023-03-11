671d71c15ec96830f99e089deb3ee9680dd2e7ec
dorreb <ashley_ure@hotmail.com>
2023-03-10 21:31:16 -0500

    refactor: remove repeated test data out into a let block to be called in each test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,53 +2,44 @@ require "rails_helper"
 
 RSpec.describe Product, type: :model do
   describe "Validations" do
-    it "saves successfully when all four fields are set" do
-      category = Category.create(name: "Flowers")
-      product =
-        Product.new(name: "Rose", price: 10.99, quantity: 5, category: category)
+    let(:category) { Category.create(name: "Flowers") }
+    let(:product) do
+      Product.new(name: "Rose", price: 10.99, quantity: 5, category: category)
+    end
+
+    it "is valid with all required fields" do
       expect(product).to be_valid
       expect(product.errors.full_messages).to be_empty
     end
 
-    it "is not valid without a name" do
-      category = Category.create(name: "Flowers")
-      product =
-        Product.new(name: nil, price: 10.99, quantity: 5, category: category)
-      expect(product).not_to be_valid
-      expect(product.errors.full_messages).to include("Name can't be blank")
-    end
+    context "when missing required fields" do
+      it "is not valid without a name" do
+        product.name = nil
+        expect(product).not_to be_valid
+        expect(product.errors.full_messages).to include("Name can't be blank")
+      end
 
-    it "is not valid without a price" do
-      category = Category.create(name: "Flowers")
-      product =
-        Product.new(
-          name: "Rose",
-          price_cents: nil,
-          quantity: 5,
-          category: category,
+      it "is not valid without a price" do
+        product.price_cents = nil
+        expect(product).not_to be_valid
+        expect(product.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it "is not valid without a quantity" do
+        product.quantity = nil
+        expect(product).not_to be_valid
+        expect(product.errors.full_messages).to include(
+          "Quantity can't be blank",
         )
-      expect(product).not_to be_valid
-      expect(product.errors.full_messages).to include("Price can't be blank")
-    end
+      end
 
-    it "is not valid without a quantity" do
-      category = Category.create(name: "Flowers")
-      product =
-        Product.new(
-          name: "Rose",
-          price: 10.99,
-          quantity: nil,
-          category: category,
+      it "is not valid without a category" do
+        product.category = nil
+        expect(product).not_to be_valid
+        expect(product.errors.full_messages).to include(
+          "Category can't be blank",
         )
-      expect(product).not_to be_valid
-      expect(product.errors.full_messages).to include("Quantity can't be blank")
-    end
-
-    it "is not valid without a category" do
-      product =
-        Product.new(name: "Rose", price: 10.99, quantity: 5, category: nil)
-      expect(product).not_to be_valid
-      expect(product.errors.full_messages).to include("Category can't be blank")
+      end
     end
   end
 end
